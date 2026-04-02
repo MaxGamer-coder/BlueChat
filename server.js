5efb4579b4039c084c5469aabc882362f00a5ce2
Tankiso <tankisotsotetsi13@gmail.com>
2026-04-02 23:33:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,42 +4,43 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { 
+  maxHttpBufferSize: 1e8 
+});
 
 app.use(express.static("public"));
 
-let typingUsers = new Set();
-let onlineUsers = new Set();
+let onlineUsers = new Map();
 
 io.on("connection", (socket) => {
-
-  socket.on("join", (username) => {
-    socket.username = username;
-    onlineUsers.add(username);
-    io.emit("message", { user: "System", text: `${username} joined the chat`, timestamp: Date.now() });
-    io.emit("onlineUpdate", Array.from(onlineUsers));
+  socket.on("join", (data) => {
+    socket.username = data.username;
+    socket.pfp = data.pfp || `https://ui-avatars.com/api/?name=${data.username}&background=random&color=fff`;
+    onlineUsers.set(socket.id, { username: socket.username, pfp: socket.pfp });
+    io.emit("onlineUpdate", Array.from(onlineUsers.values()));
   });
 
-  socket.on("chatMessage", (msg) => {
-    io.emit("message", { user: socket.username, text: msg, timestamp: Date.now() });
+  socket.on("joinPrivateRoom", (room) => { 
+    socket.join(room); 
   });
 
-  socket.on("typing", (isTyping) => {
-    if(isTyping) typingUsers.add(socket.username);
-    else typingUsers.delete(socket.username);
-    io.emit("typingUpdate", Array.from(typingUsers));
+  socket.on("privateMessage", (data) => {
+    io.to(data.room).emit("message", { 
+      user: socket.username, 
+      pfp: socket.pfp,
+      text: data.text, 
+      image: data.image, 
+      audio: data.audio,
+      room: data.room, 
+      timestamp: Date.now() 
+    });
   });
 
   socket.on("disconnect", () => {
-    if(socket.username){
-      onlineUsers.delete(socket.username);
-      typingUsers.delete(socket.username);
-      io.emit("message", { user: "System", text: `${socket.username} left the chat`, timestamp: Date.now() });
-      io.emit("typingUpdate", Array.from(typingUsers));
-      io.emit("onlineUpdate", Array.from(onlineUsers));
-    }
+    onlineUsers.delete(socket.id);
+    io.emit("onlineUpdate", Array.from(onlineUsers.values()));
   });
-
 });
 
-server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
